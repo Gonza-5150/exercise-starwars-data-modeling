9752e762f2b838e9a3edfeb4a2363adc26088a6a
Gonza-5150 <estevesgonzalo@hotmail.com>
2022-10-01 19:29:24 +0000

    relations2.1

--- src/models.py
+++ src/models.py
@@ -53,16 +53,6 @@ class Favorites_vehicles(Base):
     planet_id = Column(Integer, ForeignKey('vehicles.id'))
     
 
-class Planets(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
 
     def to_dict(self):
         return {}
