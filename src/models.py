8efb3d14c59626158868407ee4c9f20b715abcfb
Gonza-5150 <estevesgonzalo@hotmail.com>
2022-10-01 03:02:07 +0000

    Usar el excel

--- src/models.py
+++ src/models.py
@@ -9,13 +9,48 @@ from eralchemy import render_er
 Base = declarative_base()
 
 class Person(Base):
-    __tablename__ = 'person'
+    __tablename__ = 'personajes'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    geder = Column(String(250), nullable=False)
+    skin_color = Column(String(250), nullable=False)
+    birth = Column(String(250), nullable=False)
+    eye_color = Column(String(250), nullable=False)
 
-class Address(Base):
+class Planet(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    diameter = Column(String(250), nullable=False)
+    climate = Column(String(250), nullable=False)
+    population = Column(String(250), nullable=False)
+    gravity = Column(String(250), nullable=False)
+
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    fabicate = Column(String(250), nullable=False)
+    tripulation = Column(String(250), nullable=False)
+    speed = Column(String(250), nullable=False)
+    passengers = Column(String(250), nullable=False)
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(String(250), nullable=False)
+    favorito = Column(String(250), nullable=False)
+    
+
+class Planets(Base):
     __tablename__ = 'address'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
